d333afb112ea01f2bb177d9c2415ff380dcad52e
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-21 10:06:26 +0100

    genre DB deleted,addgenre model revised - NULL

--- src/genres/model.js
+++ src/genres/model.js
@@ -4,16 +4,8 @@ const connection = require("../db/connection");
 const Genre = connection.define("Genre", {
     genre: {
     type: DataTypes.STRING,
-    },
-
-    title:{ 
-        type:DataTypes.STRING,
-        // allowNull: false,
-        unique: true,
-    },
-
-    author: {
-        type:DataTypes.STRING,
+    allowNull: false,
+    unique: true,
     },
 })
 
